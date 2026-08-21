7f0eb1d861dee18cd4f88d8ab7f437e3f16506ab
abdul-kadir-uk <abdulkadiruk06@gmail.com>
2026-08-21 22:19:20 +0530

    update origin endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,6 +22,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "https://aliauf.com",
+  "https://www.aliauf.com",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
