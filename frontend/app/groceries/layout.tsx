6f4a90496cb180e88379fd61671152d997869b38
abdul-kadir-uk <abdulkadiruk06@gmail.com>
2026-08-29 20:10:54 +0530

    Add Timings

--- frontend/app/groceries/layout.tsx
+++ frontend/app/groceries/layout.tsx
@@ -10,7 +10,13 @@ export default function GroceriesLayout({
   return (
     <div className="w-full min-w-0">
       <GroceryNavbar />
-
+      <div className="text-blue-700 text-right text-sm">
+        <span>Grocery Delivery Timings: 9:00 AM to 9:00 PM.</span>
+        <span className="hidden lg:inline">
+          Orders placed after 9:00 PM will be delivered the next day after 9:00
+          AM.
+        </span>
+      </div>
       <main className="mx-auto w-full max-w-7xl min-w-0 px-2 py-3 sm:px-4 sm:py-5 lg:px-8">
         {children}
       </main>
